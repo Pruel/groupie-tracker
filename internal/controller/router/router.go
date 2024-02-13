4f2d604c9a3e8f7f898481fb047f4bcff8d563cf
dpruel <dvpruel@gmail.com>
2024-02-13 20:56:26 +0200

    Added style for our site

--- internal/controller/router/router.go
+++ internal/controller/router/router.go
@@ -24,6 +24,7 @@ func (r *Router) InitRouter() error {
 	slog.Debug("the ServeMux router initializing")
 
 	r.Mux.Handle("/view/", http.StripPrefix("/view/", http.FileServer(http.Dir("./../../view/"))))
+	http.Handle("./view/static", http.StripPrefix("./view/static/", http.FileServer(http.Dir("view/static"))))
 	// r.Mux.Handle("/view/", http.StripPrefix("/view/", http.FileServer(http.Dir(controller.GetTmplFilepath("view")))))
 	r.Mux.HandleFunc("/", controller.MainController)         //
 	r.Mux.HandleFunc("/artist", controller.ArtistController) //
